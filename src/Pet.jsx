317ef8fe53589896f10c2103beaebbb2260829db
AlanNguyen981805 <hiep.nguyen5@ntq-solution.com.vn>
2024-03-13 18:06:02 +0700

    init project

---
+++ src/Pet.jsx
@@ -0,0 +1,10 @@
+const Pet = (props) => {
+  return (
+    <div>
+      <h1>{props.animal}</h1>
+      <h2>{props.animal}</h2>
+      <h2>{props.animal}</h2>
+    </div>
+  );
+};
+export default Pet;
