97c0e5aa50efb4a116248361c217b6a54481e3bb
AlanNguyen981805 <hiep.nguyen5@ntq-solution.com.vn>
2024-03-14 15:53:29 +0700

    done couse

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,38 @@
 import React, { useState } from "react";
 import { createRoot } from "react-dom/client";
 import SearchParams from "./SearchParams";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Details from "./Details";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import AdoptedPetContext from "./AdoptedPetContext";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      cacheTime: Infinity,
+    },
+  },
+});
 
 const App = () => {
+  const adoptedPet = useState(null);
   return (
-    <div>
-      <SearchParams />
-    </div>
+    // <div>
+    <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <AdoptedPetContext.Provider value={adoptedPet}>
+          <header>
+            <Link to="/">Adopt Me!</Link>
+          </header>
+          <Routes>
+            <Route path="/details/:id" element={<Details />} />
+            <Route path="/" element={<SearchParams />} />
+          </Routes>
+        </AdoptedPetContext.Provider>
+      </QueryClientProvider>
+    </BrowserRouter>
+    // </div>
   );
 };
 
